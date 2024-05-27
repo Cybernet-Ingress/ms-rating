f6687ad5ebdf905f199f7f1694537f073f09eb4d
huseyn.huseynzada <huseyn.huseynzade@gmail.com>
2024-05-27 18:36:22 +0400

    test

---
+++ src/main/java/org/example/msraiting/MsRaitingApplication.java
@@ -0,0 +1,13 @@
+package org.example.msraiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRaitingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsRaitingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/msraiting/MsRaitingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.msraiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRaitingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
